cdec2c852160676bdbe770e559c48233c00735c0
madokamoon <1095286992@qq.com>
2025-05-27 23:11:56 +0800

    测试vit_l

--- bash_train_and_eval.sh
+++ bash_train_and_eval.sh
@@ -8,7 +8,8 @@ configs=(
     # "act_config_fwo.yaml" 
     # "act_config_bs4_s4000.yaml"
 
-    "act_sam_config_vit_h.yaml"
+    # "act_sam_config_vit_h.yaml"
+    "act_sam_config_vit_l.yaml"
 
     # "act_weight_config.yaml"
 
@@ -17,6 +18,7 @@ configs=(
 for i in "${!configs[@]}"; do
 
     config=${configs[$i]}
+    
     # python act_training.py --config-name="$config"
     python data_sampler.py --config-name="$config"
     
