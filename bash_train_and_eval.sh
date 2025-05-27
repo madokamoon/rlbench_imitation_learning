b521c71218800552ca5671c94544b0da1f677495
madokamoon <1095286992@qq.com>
2025-05-28 00:06:06 +0800

    1

--- bash_train_and_eval.sh
+++ bash_train_and_eval.sh
@@ -2,15 +2,18 @@
 
 configs=(
     
-    "act_config_f.yaml"    
-    "act_config_o.yaml"   
-    "act_config_w.yaml"  
-    "act_config_fwo.yaml" 
-    "act_config_bs4_s4000.yaml"
-    "act_sam_config_vit_h.yaml"
-    "act_sam_config_vit_l.yaml"
+    # "act_config_f.yaml"    
+    # "act_config_o.yaml"   
+    # "act_config_w.yaml"  
+    # "act_config_fwo.yaml" 
+    # "act_config_bs4_s4000.yaml"
+
+    # "act_sam_config_vit_h.yaml"
+    # "act_sam_config_vit_l.yaml"
 
     # "act_weight_config.yaml"
+
+    "act_depth_anything_config_vit_b.yaml"
 
 )
 
