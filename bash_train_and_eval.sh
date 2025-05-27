4ed4d76f6f29451eb94aefb75c4bed5b1c33ed24
madokamoon <1095286992@qq.com>
2025-05-27 23:14:38 +0800

    1

--- bash_train_and_eval.sh
+++ bash_train_and_eval.sh
@@ -2,13 +2,12 @@
 
 configs=(
     
-    # "act_config_f.yaml"    
-    # "act_config_o.yaml"   
-    # "act_config_w.yaml"  
-    # "act_config_fwo.yaml" 
-    # "act_config_bs4_s4000.yaml"
-
-    # "act_sam_config_vit_h.yaml"
+    "act_config_f.yaml"    
+    "act_config_o.yaml"   
+    "act_config_w.yaml"  
+    "act_config_fwo.yaml" 
+    "act_config_bs4_s4000.yaml"
+    "act_sam_config_vit_h.yaml"
     "act_sam_config_vit_l.yaml"
 
     # "act_weight_config.yaml"
@@ -18,7 +17,7 @@ configs=(
 for i in "${!configs[@]}"; do
 
     config=${configs[$i]}
-    
+
     # python act_training.py --config-name="$config"
     python data_sampler.py --config-name="$config"
     
